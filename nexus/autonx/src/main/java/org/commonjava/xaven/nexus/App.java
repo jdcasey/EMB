34b258ada70f5f305443e400ff6dd45e1aac6d73
John Casey <jdcasey@commonjava.org>
2010-03-31 11:51:11 -0400

    Adding some test code that queries TXT and SRV DNS records to auto-discover nexus services. Also, adding the beginnings of a Nexus plugin to retrieve all remote urls proxied by a particular group.

---
+++ nexus/autonx/src/main/java/org/commonjava/xaven/nexus/App.java
@@ -0,0 +1,13 @@
+package org.commonjava.xaven.nexus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
