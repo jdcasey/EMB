7a7eca9a8d31b2cb3c298c6b3563722841914d2b
John Casey <jdcasey@commonjava.org>
2010-08-23 13:08:56 -0400

    in-progress...

---
+++ components/version-scheme-injector/src/main/java/org/commonjava/xaven/component/vscheme/VersionSchemeManager.java
@@ -0,0 +1,47 @@
+package org.commonjava.xaven.component.vscheme;
+
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.repository.MirrorSelector;
+import org.apache.maven.settings.Mirror;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
+import org.commonjava.xaven.conf.XavenConfiguration;
+import org.commonjava.xaven.conf.XavenLibrary;
+
+import java.util.List;
+
+@Component( role = VersionSchemeManager.class )
+public class VersionSchemeManager
+    implements MirrorSelector, Initializable
+{
+
+    @SuppressWarnings( "unused" )
+    @Requirement( hint = "#" )
+    private MirrorSelector delegateSelector;
+
+    @SuppressWarnings( "unused" )
+    @Requirement( hint = "version-scheme-injector" )
+    private XavenLibrary library;
+
+    @SuppressWarnings( "unused" )
+    @Requirement
+    private XavenConfiguration xavenConfig;
+
+    @Override
+    public void initialize()
+        throws InitializationException
+    {
+        // Normally, this is where you might initialize the mirrors available to Xaven.
+        throw new UnsupportedOperationException( "Not Implemented." );
+    }
+
+    @Override
+    public Mirror getMirror( final ArtifactRepository repository, final List<Mirror> mirrors )
+    {
+        // Select the appropriate mirror from those detected.
+        throw new UnsupportedOperationException( "Not Implemented." );
+    }
+
+}
