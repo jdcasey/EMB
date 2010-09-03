6ec6ec1e9d0e9c9cadb98beaf664091beb1e0291
John Casey <jdcasey@commonjava.org>
2010-09-03 12:01:09 -0400

    more stuff I left out on last commit...getting ready to merge to master

---
+++ infrastructure/plexus-guice/src/main/java/org/sonatype/guice/plexus/locators/EMBPlexusBeanLocator.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2009 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.guice.plexus.locators;
+
+import org.sonatype.guice.bean.locators.BeanLocator;
+import org.sonatype.guice.plexus.config.PlexusBean;
+import org.sonatype.guice.plexus.config.PlexusBeanLocator;
+
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.TypeLiteral;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * {@link PlexusBeanLocator} that locates beans of various types from zero or more {@link Injector}s.
+ */
+@Singleton
+public final class EMBPlexusBeanLocator
+    implements PlexusBeanLocator
+{
+    // ----------------------------------------------------------------------
+    // Implementation fields
+    // ----------------------------------------------------------------------
+
+    private final BeanLocator beanLocator;
+
+    // ----------------------------------------------------------------------
+    // Constructors
+    // ----------------------------------------------------------------------
+
+    @Inject
+    public EMBPlexusBeanLocator( final BeanLocator beanLocator )
+    {
+        this.beanLocator = beanLocator;
+    }
+
+    // ----------------------------------------------------------------------
+    // Public methods
+    // ----------------------------------------------------------------------
+
+    public synchronized <T> Iterable<PlexusBean<T>> locate( final TypeLiteral<T> role, final String... hints )
+    {
+        final PlexusBeans<T> plexusBeans = new PlexusBeans<T>( role, hints );
+        if ( hints.length == 1 )
+        {
+            plexusBeans.setBeans( beanLocator.<Named, T> locate( Key.get( role, Names.named( hints[0] ) ), plexusBeans ) );
+        }
+        else
+        {
+            plexusBeans.setBeans( beanLocator.<Named, T> locate( Key.get( role, Named.class ), plexusBeans ) );
+        }
+        return plexusBeans;
+    }
+}
